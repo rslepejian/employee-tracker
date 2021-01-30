e3757488cc59758f44c5918dae506e94387e7781
Suda Nimh <rslepejian@gmail.com>
2021-01-29 20:40:21 -0800

    employee viewer now displays mmanager names

--- index.js
+++ index.js
@@ -31,19 +31,19 @@ connection.connect(function (err) {
 });
 
 function addDept(name) {
-    connection.query("INSERT INTO departments SET ?", {name: name}, function (err, result) {
+    connection.query("INSERT INTO departments SET ?", { name: name }, function (err, result) {
         if (err) throw err;
     });
 }
 
 function addRole(title, salary, department_id) {
-    connection.query("INSERT INTO roles SET ?", {title: title, salary: salary, department_id: department_id}, function (err, result) {
+    connection.query("INSERT INTO roles SET ?", { title: title, salary: salary, department_id: department_id }, function (err, result) {
         if (err) throw err;
     });
 }
 
 function addEmpl(first_name, last_name, role_id, manager_id) {
-    connection.query("INSERT INTO employees SET ?", {first_name: first_name, last_name: last_name, role_id: role_id, manager_id: manager_id}, function (err, result) {
+    connection.query("INSERT INTO employees SET ?", { first_name: first_name, last_name: last_name, role_id: role_id, manager_id: manager_id }, function (err, result) {
         if (err) throw err;
     });
 }
@@ -57,7 +57,9 @@ function viewDept() {
 }
 
 function viewRole() {
-    connection.query("SELECT * FROM roles;", function (err, data) {
+    // find names of departments based on ids
+    // find names of managers based on ids
+    connection.query("SELECT departments.name, roles.title, roles.salary FROM departments INNER JOiN roles ON departments.id = roles.department_id;", function (err, data) {
         if (err) throw err;
         newTable = cTable.getTable(data);
         console.table(newTable);
@@ -65,28 +67,33 @@ function viewRole() {
 }
 
 function viewEmpl() {
-    connection.query("SELECT * FROM employees;", function (err, data) {
+    // find names of roles based on ids
+    // find name of manager based on id?
+    connection.query(`SELECT e1.first_name, e1.last_name, CONCAT(e2.first_name, ' ', e2.last_name) AS 'Manager', r.title FROM roles as r INNER JOIN employees as e1 ON e1.role_id = r.id LEFT JOIN employees as e2 ON e1.manager_id = e2.id`, function (err, data) {
         if (err) throw err;
         newTable = cTable.getTable(data);
         console.table(newTable);
     });
 }
 
-function updateEmpl(newRole, newDept, first_name, last_name) {
+function updateEmpl(newRoleId, firstname, lastname, id) {
     // find the id of the new role
     // find the department id of the new role
     // set the employee's role id to the role id
     // set the employees department id to the new one
-    connection.query("UPDATE employees SET role = ? WHERE ?", {roleid: newRoleId, department_id: newDeptId},{first_name: first_name, last_name: last_name},function (err, data) {
+    connection.query("UPDATE `employees` SET ? WHERE ? AND ? AND ?;", [{role_id: newRoleId}, {first_name: firstname}, {last_name: lastname}, {id: id}], function (err, data) {
         if (err) throw err;
     });
 }
-addDept("quality assurance");
-addRole("quality tester", 15, 4);
-addEmpl("qual", "testy", 4, 1);
+// addDept("quality assurance");
+// addRole("quality tester", 15, 4);
+// addEmpl("qual", "testy", 4, 1);
 
+// updateEmpl(2, "tech", "nician", 2);
 
-viewDept();
+// viewDept();
 viewEmpl();
-viewRole();
+// viewRole();
+
+
 
