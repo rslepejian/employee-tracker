b5d5f3c2874e56709685be444ea7a69dbc87c675
Suda Nimh <rslepejian@gmail.com>
2021-01-29 18:50:00 -0800

    add functions now successfully add to the sql database

--- index.js
+++ index.js
@@ -31,19 +31,19 @@ connection.connect(function (err) {
 });
 
 function addDept(name) {
-    connection.query("INSERT INTO departments VALUES (?)", {name: name}, function (err, result) {
+    connection.query("INSERT INTO departments SET ?", {name: name}, function (err, result) {
         if (err) throw err;
     });
 }
 
 function addRole(title, salary, department_id) {
-    connection.query("INSERT INTO roles VALUES (?)", {title: title, salary: salary, department_id: department_id}, function (err, result) {
+    connection.query("INSERT INTO roles SET ?", {title: title, salary: salary, department_id: department_id}, function (err, result) {
         if (err) throw err;
     });
 }
 
 function addEmpl(first_name, last_name, role_id, manager_id) {
-    connection.query("INSERT INTO employees VALUES (?)", {first_name: first_name, last_name: last_name, role_id: role_id, manager_id: manager_id}, function (err, result) {
+    connection.query("INSERT INTO employees SET ?", {first_name: first_name, last_name: last_name, role_id: role_id, manager_id: manager_id}, function (err, result) {
         if (err) throw err;
     });
 }
@@ -51,23 +51,42 @@ function addEmpl(first_name, last_name, role_id, manager_id) {
 function viewDept() {
     connection.query("SELECT * FROM departments;", function (err, data) {
         if (err) throw err;
+        newTable = cTable.getTable(data);
+        console.table(newTable);
     });
 }
 
 function viewRole() {
     connection.query("SELECT * FROM roles;", function (err, data) {
         if (err) throw err;
+        newTable = cTable.getTable(data);
+        console.table(newTable);
     });
 }
 
 function viewEmpl() {
     connection.query("SELECT * FROM employees;", function (err, data) {
         if (err) throw err;
+        newTable = cTable.getTable(data);
+        console.table(newTable);
     });
 }
 
-function updateEmpl(newRole, id) {
-    connection.query("UPDATE employees SET role = " + newRole + "WHERE id = " + id, function (err, data) {
+function updateEmpl(newRole, newDept, first_name, last_name) {
+    // find the id of the new role
+    // find the department id of the new role
+    // set the employee's role id to the role id
+    // set the employees department id to the new one
+    connection.query("UPDATE employees SET role = ? WHERE ?", {roleid: newRoleId, department_id: newDeptId},{first_name: first_name, last_name: last_name},function (err, data) {
         if (err) throw err;
     });
 }
+addDept("quality assurance");
+addRole("quality tester", 15, 4);
+addEmpl("qual", "testy", 4, 1);
+
+
+viewDept();
+viewEmpl();
+viewRole();
+
